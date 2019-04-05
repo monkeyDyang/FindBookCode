c0667a400135cf93649410083dc9f4ea09964a63
yang <1059376437@qq.com>
2019-04-05 19:49:13 +0800

    对database进行测试，测试成功

--- PythonCode/Spiderman/Spiderman/database.py
+++ PythonCode/Spiderman/Spiderman/database.py
@@ -3,11 +3,12 @@
  * @Author: mikey.zhaopeng 
  * @Date: 2019-04-05 16:06:05 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2019-04-05 16:53:12
+ * @Last Modified time: 2019-04-05 18:47:57
  */
 """
 
 import pymysql
+import pymysql.cursors
 
 MYSQL_DB = 'findbook'
 MYSQL_USER = 'root'
@@ -15,8 +16,24 @@ MYSQL_PASS = '123456'
 MYSQL_HOST = '118.25.195.70'
 
 connection = pymysql.connect(host=MYSQL_HOST, 
+                             port = 3306,
                              user=MYSQL_USER,
                              password=MYSQL_PASS, 
                              db=MYSQL_DB,
                              charset='utf8',
                              cursorclass=pymysql.cursors.DictCursor)
+
+
+# DBConnection_test
+"""
+cursors = connection.cursor()
+sql = 'insert into test value (1,"ss")'
+cursors.execute(sql)
+connection.commit()
+print("********")
+print(cursors.rowcount)
+print("********")
+cursors.close()
+connection.close()
+"""
+# 测试结果 成功向 test表插入一条数据，返回1
